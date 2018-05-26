474d9929bf75454f1751167768eb3bb62c6ccc81
lopolafa <brandonburgoafigueroa@gmail.com>
2018-05-26 18:36:55 -0400

    asd

---
+++ Web App Arqui/Pages/Connect.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_App_Arqui.Pages
+{
+    public class InitialPromtModel : PageModel
+    {
+        [BindProperty]
+        public string Message { set; get; }
+        [BindProperty]
+        public string IDMailbox { set; get; }
+        private static string MessageInitial = "Enter mailbox number followed by #";
+        private static string MessageErrorConnection = "Incorrect mailbox number. Try again!";
+
+        public void OnGet()
+        {
+            Message = MessageInitial;
+        }
+        public IActionResult OnPost()
+        {
+            bool result = simulate();
+            if (result)
+                return RedirectToPage("/Recording");
+            Message = MessageErrorConnection;
+            return Page();
+        }
+
+        /*references*/
+        public bool simulate()
+        {
+            return IDMailbox==Convert.ToString(1);
+        }
+    }
+}
---
+++ Web App Arqui/Pages/Recording.cshtml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_App_Arqui.Pages
+{
+    public class RecordingModel : PageModel
+    {
+
+        [BindProperty]
+        public string Message { set; get; }
+
+        [BindProperty]
+        public string Passcode { set; get; }
+        [BindProperty]
+        public string Error { set; get; }
+        [BindProperty]
+        public string Greeting { set; get; }
+        public void OnGet()
+        {
+            Mailbox Mailbox = new Mailbox();
+            Greeting = Mailbox.Greeting;
+        }
+        public IActionResult OnPostSendMessage()
+        {
+            //añadir mensage
+            bool result = true;
+            if (result)
+                return RedirectToPage("/Connect");
+            Error = "Algo sucedio";
+            return Page();
+            
+        }
+        public IActionResult OnPostLogin()
+        {
+            //estado
+            bool result = Passcode == Convert.ToString(1);
+            if (result)
+                return RedirectToPage("/MailboxMenu");
+            Error = "Incorrect passcode. Try again!";
+            return Page();
+        }
+    }
+    public class Mailbox
+    {
+        public MessageQueue NewMessages { set; get; }
+        public MessageQueue KeptMessages { set; get; }
+        public string Passcode { set; get; }
+        public string Greeting { set; get; }
+
+    }
+    public class MessageQueue
+    {
+        public List<Message> Queue { set; get; }
+    }
+    public class Message
+    {
+        public string From { set; get; }
+
+        public string Text { set; get; }
+    }
+}
